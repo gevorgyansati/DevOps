f1808cb0cec34be9d3be609209b522270f7a17eb
hakobmkoyan771 <95763847+hakobmkoyan771@users.noreply.github.com>
2022-05-20 21:34:39 +0400

    Update createDevData.sh

--- createDevData.sh
+++ createDevData.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$1" > ../repoinfo.txt #TAG
-echo "$2" >> ../repoinfo.txt #REPO
-echo "$3" >> ../repoinfo.txt #COMMIT
+echo -n "$1" > ../repoinfo.txt #TAG
+echo -n "$2" >> ../repoinfo.txt #REPO
+echo -n "$3" >> ../repoinfo.txt #COMMIT
